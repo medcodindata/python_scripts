ac4cff92ff14ccd658889d6face14c5f95b92c59
medcodindata <56320141+medcodindata@users.noreply.github.com>
2019-11-03 17:18:33 +0100

    Create sum_of_series.py

---
+++ sum_of_series.py
@@ -0,0 +1,32 @@
+# sum of the n first element of this series  x -  x**3/3! + x**5/n! -... for given x
+# and display the detail of the series as string (displaying the value of each term of the series).
+
+
+from math import factorial
+
+x = int(input("Input the value of x: "))
+n = int(input("Input the value of n: "))
+output = str(x)
+k = 3
+s = int(x)
+
+for i in range(1,n):
+    sign = (-1)**i
+    term = x ** k / factorial(k)
+    element = " + " + str(round(abs(term), 2))
+    if term*sign < 0 :
+        element = " - " + str(round(abs(term),2))
+    output += element
+    s += term * sign
+    k += 2
+    
+output += " = " + str(round(s,2))
+print('The sum of the series are:')
+print(output)
+
+# Example of x = 7, n = 6:
+
+Input the value of x: 7
+Input the value of n: 6
+The sum of the series are:
+7 - 57.17 + 140.06 - 163.4 + 111.2 - 49.54 = -11.84
